cb006d1cc6026a494bf298ce57a2e049162e01b3
garrettlarson-dev <larsonwgarrett@gmail.com>
2024-01-17 15:19:51 -0700

    Commented Code

--- Program.cs
+++ Program.cs
@@ -2,33 +2,40 @@
 
 internal class Program
 {
-    private static void Main(string[] args) 
+    private static void Main(string[] args)
     {
-        
-
+        // Welcome message
         System.Console.WriteLine("Welcome to the dice throwing simulator!");
-        System.Console.WriteLine("How many dice rolls would like to simulate?: ");
+        System.Console.WriteLine("How many dice rolls would you like to simulate?: ");
 
+        // Get user input for the number of dice rolls
         int numRolls = int.Parse(System.Console.ReadLine());
         RollDice rd = new RollDice(numRolls);
         int[] results = rd.roll();
 
+        // Display simulation results header
         System.Console.WriteLine("DICE ROLLING SIMULATION RESULTS");
-        System.Console.WriteLine("Each '*' represents 1% of the total numer of rolls.");
+        System.Console.WriteLine("Each '*' represents 1% of the total number of rolls.");
         System.Console.WriteLine("Total number of rolls = " + numRolls);
 
+        // Display results with percentage representation using '*' characters
         for (int i = 0; i < results.Length - 1; i++)
         {
             string stars = "";
+
+            // Calculate the percentage for each result
             float percentageToAdd = ((float)results[i] / numRolls) * 100;
+
+            // Generate a string of '*' characters based on the calculated percentage
             for (int j = 0; j < percentageToAdd; j++)
             {
                 stars += "*";
             }
 
-            string percentageString = percentageToAdd.ToString("0.0"); // Format the percentage with two decimal places
+            // Format and display the result with percentage and '*' representation
+            string percentageString = percentageToAdd.ToString("0.0");
 
-            if ((i+2) < 10)
+            if ((i + 2) < 10)
             {
                 System.Console.WriteLine("(" + percentageString + "%)\t" + " " + (i + 2) + ": " + stars);
             }
@@ -37,8 +44,8 @@ internal class Program
                 System.Console.WriteLine("(" + percentageString + "%)\t" + (i + 2) + ": " + stars);
             }
         }
+
+        // Farewell message
         System.Console.WriteLine("Thank you for using the dice throwing simulator. Goodbye!");
-
-
     }
 }
--- RollDice.cs
+++ RollDice.cs
@@ -6,8 +6,10 @@ using System.Threading.Tasks;
 
 namespace mission2
 {
+    // Class responsible for simulating dice rolls
     internal class RollDice
     {
+        // Fields to store information about the dice rolls
         int totalNumRolls;
         int rollsLeft;
         int dice1;
@@ -15,35 +17,40 @@ namespace mission2
         int sum;
         int[] combinations;
 
-        public RollDice(int numRolls) {
+        // Constructor to initialize the RollDice object with the total number of rolls
+        public RollDice(int numRolls)
+        {
+            // Initialize array to store combinations and set the total number of rolls
             combinations = new int[11];
             totalNumRolls = numRolls;
             rollsLeft = numRolls;
         }
+
+        // Method to simulate dice rolls and return an array with combinations
         public int[] roll()
         {
+            // Create a random number generator
             System.Random random = new System.Random();
 
+            // Simulate dice rolls until there are no rolls left
             while (rollsLeft > 0)
             {
+                // Generate random numbers for two dice
                 dice1 = random.Next(1, 7);
                 dice2 = random.Next(1, 7);
+
+                // Calculate the sum of the two dice
                 sum = dice1 + dice2;
 
-                /* Console.WriteLine("Dice sum is " + sum); */
-
+                // Increment the corresponding combination in the array
                 combinations[sum - 2]++;
 
-                /* for (int i = 0; i < combinations.Length; i++)
-                {
-                    Console.WriteLine(combinations[i]);
-                }
-                 */
+                // Decrease the number of rolls left
                 rollsLeft--;
             }
 
+            // Return the array containing the combinations of dice rolls
             return combinations;
-
         }
     }
 }
