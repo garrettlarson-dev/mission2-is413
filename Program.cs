6a1181079bd7c768108beee7f4c86bbe39b0558a
garrettlarson <garrettlarson@GARRETTLARSA6FA>
2024-01-17 13:01:55 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using mission2;
+
+internal class Program
+{
+    private static void Main(string[] args) 
+    {
+        
+
+        System.Console.WriteLine("Welcome to the dice throwing simulator!");
+        System.Console.WriteLine("How many dice rolls would like to simulate?: ");
+
+        int numRolls = int.Parse(System.Console.ReadLine());
+        RollDice rd = new RollDice(numRolls);
+        int[] results = rd.roll();
+
+        System.Console.WriteLine("DICE ROLLING SIMULATION RESULTS");
+        System.Console.WriteLine("Each '*' represents 1% of the total numer of rolls.");
+        System.Console.WriteLine("Total number of rolls = " + numRolls);
+
+        for (int i = 0; i < results.Length - 1; i++)
+        {
+            string stars = "";
+            float percentageToAdd = ((float)results[i] / numRolls) * 100;
+            for (int j = 0; j < percentageToAdd; j++)
+            {
+                stars += "*";
+            }
+
+            string percentageString = percentageToAdd.ToString("0.0"); // Format the percentage with two decimal places
+
+            if ((i+2) < 10)
+            {
+                System.Console.WriteLine("(" + percentageString + "%)\t" + " " + (i + 2) + ": " + stars);
+            }
+            else
+            {
+                System.Console.WriteLine("(" + percentageString + "%)\t" + (i + 2) + ": " + stars);
+            }
+        }
+        System.Console.WriteLine("Thank you for using the dice throwing simulator. Goodbye!");
+
+
+    }
+}
---
+++ RollDice.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mission2
+{
+    internal class RollDice
+    {
+        int totalNumRolls;
+        int rollsLeft;
+        int dice1;
+        int dice2;
+        int sum;
+        int[] combinations;
+
+        public RollDice(int numRolls) {
+            combinations = new int[11];
+            totalNumRolls = numRolls;
+            rollsLeft = numRolls;
+        }
+        public int[] roll()
+        {
+            System.Random random = new System.Random();
+
+            while (rollsLeft > 0)
+            {
+                dice1 = random.Next(1, 7);
+                dice2 = random.Next(1, 7);
+                sum = dice1 + dice2;
+
+                /* Console.WriteLine("Dice sum is " + sum); */
+
+                combinations[sum - 2]++;
+
+                /* for (int i = 0; i < combinations.Length; i++)
+                {
+                    Console.WriteLine(combinations[i]);
+                }
+                 */
+                rollsLeft--;
+            }
+
+            return combinations;
+
+        }
+    }
+}
